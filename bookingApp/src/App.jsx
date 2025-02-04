7872fac8432154026fa2c65aca6f4748992366b6
Rae Wiechens <raeannw321@gmail.com>
2025-02-03 20:02:32 -0600

    adding code

---
+++ bookingApp/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./Layout";
+import RegisterPage from "./pages/RegisterPage";
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://localhost:4000';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
