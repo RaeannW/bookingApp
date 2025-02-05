65710e7cf5629d37749df0014cedaf215de5ba5c
Rae Wiechens <raeannw321@gmail.com>
2025-02-04 22:21:09 -0600

    setting up mongodb/mongoose

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const { default: mongoose } = require('mongoose');
+const bcrypt = require('bcryptjs')
+const User = require('./models/user.js')
+require('dotenv').config()
+const app = express()
+
+const secret = bcrypt.genSaltSync(10);
 
 app.use(express.json())
 
@@ -9,14 +15,23 @@ app.use(cors({
     origin: 'http://localhost:5173'
 }));
 
+mongoose.connect(process.env.MONGO_URL);
+
 // Define a route
 app.get('/', (req, res) => {
     res.json('testing');
 });
 
-app.post('/register', (req, res) => {
+
+app.post('/register', async (req, res) => {
     const {name, email, password} = req.body;
-    res.json({name, email, password})
+    const userData = await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password, secret),
+    })
+
+    res.json(userData);
 })
 
 // Start the server
